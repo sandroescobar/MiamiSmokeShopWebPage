44a89fdbf326563fc1705596a76d90cd32fd782c
Alessandro Escobar <aesco115@fiu.edu>
2026-02-18 22:49:09 -0500

    age checker null fix 5

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-const AGECHECKER_ACCOUNT_HASH = process.env.AGECHECKER_ACCOUNT_SECRET || '21LdVe4lD0My3QjL';
+const AGECHECKER_ACCOUNT_HASH = process.env.AGECHECKER_ACCOUNT_HASH || '21LdVe4lD0My3QjL';
+const AGECHECKER_ACCOUNT_SECRET = process.env.AGECHECKER_ACCOUNT_SECRET || '21LdVe4lD0My3QjL';
 const AGECHECKER_API_KEY = process.env.AGECHECKER_API_KEY_DELIVERY || '9g2Z8WhFz2LhbQANYf8YjPf7930jzSrY';
 
 app.get('/agechecker/init.js', async (req, res) => {
@@ -3271,7 +3272,7 @@ app.post('/api/authorize/charge', async (req, res) => {
       `https://api.agechecker.net/v1/status/${uuid}`,
       {
         headers: {
-          "X-AgeChecker-Secret": AGECHECKER_ACCOUNT_HASH
+          "X-AgeChecker-Secret": AGECHECKER_ACCOUNT_SECRET
         }
       }
     );
